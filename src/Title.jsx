921b5bc2087bf95fca251aeeb328df81fdc6e749
Daniel Brüggemann <github@barfooz.com>
2017-10-26 15:05:08 +0200

    Init

---
+++ src/Title.jsx
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export default styled.h1`
+    color: rgb(0, 124, 187);
+    font-family: monospace;
+    font-weight: 400;
+`;
---
+++ src/Translations/defaultMessages.js
@@ -0,0 +1,10 @@
+export default {
+    en: {
+        title: 'Simple language picker',
+        toggler: 'Toggle language',
+    },
+    de: {
+        title: 'Einfache Sprachauswahl',
+        toggler: 'Sprache umschalten',
+    },
+};
